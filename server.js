c1a38eab8d4ff7a59f7d1330a6aa6f7de3dab38b
Jonathan Smith <jonsmj@gmail.com>
2019-02-14 16:22:26 -0700

    Finished Judge game board

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+const ROOM_CODE_LEN = 5
 //load core modules
 const express = require('express')
 
@@ -33,9 +34,6 @@ app.use(bodyParser.json())
 // API Calls will go in the api-routes.js file AND MUST BE AFTER THE PARSERS
 app.use('/api', apiRoutes)
 
-app.get('*', function(req, res) {
-	res.status(404).sendFile(`${__dirname}/web/404.html`)
-})
 
 //start the server
 const server = app.listen(8080, process.env.IP, 511, function() {
@@ -51,67 +49,144 @@ io.on('connection', (socket) => {
 	console.log('User connected')
 
 	socket.on('join', function(data) {
+// Check if the room exists
+		if (data.room in games){
+			// Check if room already has a judge
+			if (data.type === "judge"){
+				if(games[data.room]["judge"]){
+					console.log("Game already has a judge!")
+					socket.emit('er',"alreadyJudge")
+				}else{
+					console.log(`Judge joined room ${data.room}`)
+					socket.join(data.room);
 
-		let game = {}
+					games[data.room]["judge"] = socket
 
-		if (data.type === "judge"){
-			console.log(`Judge joined room ${data.room}`)
-			socket.join(data.room);
+					socket.emit('command', 'startGame')
+				}
+			}else if(type === "screen"){
+				console.log(`screen joined room ${data.room}`)
+				socket.join(data.room);
+				games[data.room]["screen"] = socket
 
-			game["judge"] = socket
+				if (games[data.room][team1Pts] > 0 || games[data.room][team2Pts] > 0)
+				{
+					console.log("Game already in progress. Sending current game info to client")
+					socket.emit('gameInProgress',games[room]["gameData"])
+				}
 
-		}else if(type === "screen"){
-			console.log(`screen joined room ${data.room}`)
-			socket.join(data.room);
-			game["screen"] = socket
+				socket.emit('command', 'startGame')
 
-			if (games[data.room][team1Pts] > 0 || games[data.room][team2Pts] > 0)
-			{
-				console.log("Game already in progress. Sending current game info to client")
-				soket.emit('gameInProgress',games[room])
+			}else{
+				console.log("Unknown client type, disconnect from client")
+				socket.disconnect(true)
 			}
-
 		}else{
-			console.log("Unknown client type, disconnect from client")
-		}
-
-		if (!games[data.room]){
-			games[data.room] = game
-			games[data.room]['team1'] = 0
-			games[data.room]['team2'] = 0
+			console.log("Game does not exists")
+			socket.emit('er',"noGameFound")
 		}
 	})
 
 	socket.on('questionID', function(data) {
 		console.log(`Updating Question ID for room ${data.room}`)
 
-		games[data.room]["questionID"] = data.questionID
+		games[data.room]["gameData"]["questionID"] = data.questionID
 
 		console.log("Sending updated question to clients")
 
 		socket.to('data.room').emit('questionID', data.questionID);
+
+		// New question, if there are any recorded card flips delete them
+		if(games[data.room]["gameData"]["cardFlips"]){
+			delete games[data.room]["gameData"]["cardFlips"]
+		}
 	})
 
 	socket.on('commands', function(data) {
 		console.log(`Sending command ${Object.keys(data.command)} to room ${data.room}`)
-
 		socket.to('data.room').emit('questionID', data.command);
+		
+		// Keep track of carde flips
+		if (data.command.cardFlip)
+		{
+			let cardsFlipped = []
 
+			if (games[data.room]["gameData"]["cardFlips"]){
+				cardsFlipped = games[data.room]["gameData"]["cardFlips"]
+			}
+
+			cardsFlipped.push(data.command["cardFlip"])
+
+			games[data.room]["gameData"]["cardFlips"] = cardsFlipped
+
+			console.log(games[data.room]["gameData"]["cardFlips"])
+		}
 	})
 
 	socket.on('points', function(data) {
-		console.log(data)
 		console.log(`Updating points for room ${data.room}`)
 
-		games[data.room][data.points.teamID] = data.points.pts
+		games[data.room]["gameData"][data.points.teamID] = data.points.pts
 
-		console.log(`Team 1: ${games[data.room]['team1']} Team 2: ${games[data.room]['team2']}`)
+		console.log(`Team 1: ${games[data.room]["gameData"]['team1']} Team 2: ${games[data.room]["gameData"]['team2']}`)
 
 	})
 
 	socket.on('disconnect', () => {
 		console.log('User disconnected')
+
+// Check if the judge disconnects. If so delete the game object from games array
+		let gameToRemove = ""
+		for(game in games){
+			console.log(game)
+			if (socket === games[game]["judge"]){
+				gameToRemove = game
+				break;
+			}
+		}
+
+		if (gameToRemove.length > 0){
+			console.log(`Judge disconnected from game ${gameToRemove} delete this game`)
+			delete games[gameToRemove]
+		}
 	})
+})
+
+function makeRoomCode() {
+	let text = ""
+	let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	do
+	{
+		for (var i = 0; i < ROOM_CODE_LEN; i++){
+			text += possible.charAt(Math.floor(Math.random() * possible.length))
+		}
+	} while(text in games)
+
+	return text;
+}
+
+// Create a new Game
+app.get('/game/newgame', function(req, res) {
+	let game = {}
+
+	let room = makeRoomCode()
+
+	game["gameData"] = {}
+
+	console.log(room)
+
+	game["gameData"]['team1'] = 0
+	game["gameData"]['team2'] = 0
+
+	games[room] = game
+
+	res.redirect(`/game/judge.html?room=${room}`)
+
+})
+
+app.get('*', function(req, res) {
+	res.status(404).sendFile(`${__dirname}/web/404.html`)
 })
 
 
--- web/game/js/judge.js
+++ web/game/js/judge.js
@@ -10,6 +10,28 @@ socket.on('connect', function() {
 
 })
 
+socket.on('er', function(data) {
+  console.log("Connected to SocketIo Server")
+  
+    if (data === "noGameFound"){
+	    console.log("Not a valid room code!")
+	    socket.disconnect()
+	    
+	    alert('This room is not found! Please create a new game if you want to play.');
+	    window.location.href = '/game/new_game.html'
+	    
+	}
+	if (data === "alreadyJudge"){
+	    console.log("There is already a judge in this game!")
+	    socket.disconnect()
+	    
+	    alert('There is already a judge in this game!');
+	    window.location.href = '/game/new_game.html'
+	    
+	}
+
+})
+
 socket.on('command', function (data) {
   console.log(data)
   
@@ -18,6 +40,11 @@ socket.on('command', function (data) {
    console.log("Server wants me to disconnect. Disconnecting")
    socket.disconnect()
  }
+ if(data === "startGame")
+ {
+	 loadGame()
+ }
+ 
 })
 
 function sendQuestionID(question){
@@ -33,197 +60,204 @@ function sendQuestionID(question){
 })
 }
 
-let app = {
-  version: 1,
-  currentQ: 0,
-  jsonFile:`http://${window.location.host}/api/questions`,
-  board: $("<div class='gameBoard'>"+
-   
-   "<!--- Scores --->"+
-   "<div class='score' id='boardScore'>0</div>"+
-   "<div class='score' id='team1' >0</div>"+
-   "<div class='score' id='team2' >0</div>"+
-   
-   "<!--- Question --->"+
-   "<div class='questionHolder'>"+
-   "<span class='question'></span>"+
-   "</div>"+
-   
-   "<!--- Answers --->"+
-   "<div class='colHolder'>"+
-   "<div class='col1'></div>"+
-   "<div class='col2'></div>"+
-   "</div>"+
-   
-   "<!--- Buttons --->"+
-   "<div class='btnHolder'>"+
-   "<div id='awardTeam1' data-team='1' class='button'>Award Team 1</div>"+
-   "<div id='newQuestion' class='button'>New Question</div>"+
-   "<div id='awardTeam2' data-team='2'class='button'>Award Team 2</div>"+
-   "</div>"+
-   
-   "</div>"),
-  // Utility functions
-  shuffle: function(array){
-    let currentIndex = array.length, temporaryValue, randomIndex
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex)
-      currentIndex -= 1
-      temporaryValue = array[currentIndex]
-      array[currentIndex] = array[randomIndex]
-      array[randomIndex] = temporaryValue
-    }
-    return array;
-  },
-  jsonLoaded: function(data){
-    let newData = []
-    
-    for (let key in data) {
-     let question = []
-     let answer = []
-     
-     for (let ans in data[key]['answer']){
-       let a = []
-       a[0] = data[key]['answer'][ans]['text']
-       a[1] = data[key]['answer'][ans]['pts']
-       
-       answer.push(a)
-     }
-     newData[data[key]['question']] = answer
-   }
-   
-   
-   app.allData = newData
-   app.questions = Object.keys(newData)
-   app.shuffle(app.questions)
-   app.makeQuestion(app.currentQ)
-   $('body').append(app.board)
- },
-  // Action functions
-  makeQuestion: function(qNum){
-    let qText  = app.questions[qNum]
-    
-    sendQuestionID(qText)
-    
-    let qAnswr = app.allData[qText]
+function loadGame(){
 
-    var qNum = qAnswr.length
-    qNum = (qNum<8)? 8: qNum
-    qNum = (qNum % 2 != 0) ? qNum+1: qNum
-    
-    let boardScore = app.board.find("#boardScore")
-    let question   = app.board.find(".question")
-    let col1       = app.board.find(".col1")
-    let col2       = app.board.find(".col2")
-    
-    boardScore.html(0)
-    question.html(qText.replace(/&x22;/gi,'"'))
-    col1.empty()
-    col2.empty()
+	let app = {
+	  version: 1,
+	  currentQ: 0,
+	  jsonFile:`http://${window.location.host}/api/questions`,
+	  board: $(`<div class='gameBoard'>`+
+	   
+	   `<!--- Scores --->`+
+	   `<div class='score' id='boardScore'>0</div>`+
+	   `<div class='score' id='team1' >0</div>`+
+	   `<div class='score' id='team2' >0</div>`+
+	   
+	   `<!--- Question --->`+
+	   `<div class='questionHolder'>`+
+	   `<span class='question'></span>`+
+	   `</div>`+
+	   
+	   `<!--- Answers --->`+
+	   `<div class='colHolder'>`+
+	   `<div class='col1'></div>`+
+	   `<div class='col2'></div>`+
+	   `</div>`+
+	   
+	   `<!--- Buttons --->`+
+	   `<div class='btnHolder'>`+
+	   `<div id='awardTeam1' data-team='1' class='button'>Award Team 1</div>`+
+	   `<div id='newQuestion' class='button'>New Question</div>`+
+	   `<div id='awardTeam2' data-team='2'class='button'>Award Team 2</div>`+
+	   `<div id='roomCode'>Room Code: ${roomNum}</div?`+
+	   `</div>`+
 
-    for (let i = 0; i < qNum; i++){
-      let aLI     
-      if(qAnswr[i]){
-        aLI = $("<div class='cardHolder'>"+
-          `<div id='card${i}' class='card'>`+
-          "<div class='front'>"+
-          "<span class='DBG'>"+(i+1)+"</span>"+
-          "</div>"+
-          "<div class='back DBG'>"+
-          "<span>"+qAnswr[i][0]+"</span>"+
-          "<b class='LBG'>"+qAnswr[i][1]+"</b>"+
-          "</div>"+
-          "</div>"+
-          "</div>")
-      } else {
-        aLI = $("<div class='cardHolder empty'><div></div></div>")
-      }
-      let parentDiv = (i<(qNum/2))? col1: col2
-      $(aLI).appendTo(parentDiv)
-    }  
-    
-    let cardHolders = app.board.find('.cardHolder')
-    let cards       = app.board.find('.card')
-    let backs       = app.board.find('.back')
-    let cardSides   = app.board.find('.card>div')
+	   `</div>`),
+	  // Utility functions
+	  shuffle: function(array){
+	    let currentIndex = array.length, temporaryValue, randomIndex
+	    while (0 !== currentIndex) {
+	      randomIndex = Math.floor(Math.random() * currentIndex)
+	      currentIndex -= 1
+	      temporaryValue = array[currentIndex]
+	      array[currentIndex] = array[randomIndex]
+	      array[randomIndex] = temporaryValue
+	    }
+	    return array;
+	  },
+	  jsonLoaded: function(data){
+	    let newData = []
+	    
+	    for (let key in data) {
+	     let question = []
+	     let answer = []
+	     
+	     for (let ans in data[key]['answer']){
+	       let a = []
+	       a[0] = data[key]['answer'][ans]['text']
+	       a[1] = data[key]['answer'][ans]['pts']
+	       
+	       answer.push(a)
+	     }
+	     newData[data[key]['question']] = answer
+	   }
+	   
+	   
+	   app.allData = newData
+	   app.questions = Object.keys(newData)
+	   app.shuffle(app.questions)
+	   app.makeQuestion(app.currentQ)
+	   $('body').append(app.board)
+	 },
+	  // Action functions
+	  makeQuestion: function(qNum){
+	    let qText  = app.questions[qNum]
+	    
+	    sendQuestionID(qText)
+	    
+	    let qAnswr = app.allData[qText]
+	
+	    var qNum = qAnswr.length
+	    qNum = (qNum<8)? 8: qNum
+	    qNum = (qNum % 2 != 0) ? qNum+1: qNum
+	    
+	    let boardScore = app.board.find("#boardScore")
+	    let question   = app.board.find(".question")
+	    let col1       = app.board.find(".col1")
+	    let col2       = app.board.find(".col2")
+	    
+	    boardScore.html(0)
+	    question.html(qText.replace(/&x22;/gi,'"'))
+	    col1.empty()
+	    col2.empty()
+	
+	    for (let i = 0; i < qNum; i++){
+	      let aLI     
+	      if(qAnswr[i]){
+	        aLI = $("<div class='cardHolder'>"+
+	          `<div id='card${i}' class='card'>`+
+	          "<div class='front'>"+
+	          "<span>"+qAnswr[i][0]+"</span>"+
+	           "<b>"+qAnswr[i][1]+"</b>"+
+	          "</div>"+
+	          "<div class='back DBG' style='border-style: solid; border-color: yellow;'>"+
+	          "<span>"+qAnswr[i][0]+"</span>"+
+	          "<b class='LBG'>"+qAnswr[i][1]+"</b>"+
+	          "</div>"+
+	          "</div>"+
+	          "</div>")
+	      } else {
+	        aLI = $("<div class='cardHolder empty'><div></div></div>")
+	      }
+	      let parentDiv = (i<(qNum/2))? col1: col2
+	      $(aLI).appendTo(parentDiv)
+	    }  
+	    
+	    let cardHolders = app.board.find('.cardHolder')
+	    let cards       = app.board.find('.card')
+	    let backs       = app.board.find('.back')
+	    let cardSides   = app.board.find('.card>div')
+	
+	    TweenLite.set(cardHolders , {perspective:800})
+	    TweenLite.set(cards       , {transformStyle:"preserve-3d"})
+	    TweenLite.set(backs       , {rotationX:180})
+	    TweenLite.set(cardSides   , {backfaceVisibility:"hidden"})
+	
+	    cards.data("flipped", false)
+	    
+	    function showCard(){
+	      let card = $('.card', this) 
+	      let flipped = $(card).data("flipped")
+	      let cardRotate = (flipped)?0:-180;
+	      TweenLite.to(card, 1, {rotationX:cardRotate, ease:Back.easeOut})
+	      flipped = !flipped
+	      $(card).data("flipped", flipped)
+	      socket.emit('commands', { room: roomNum, command:{cardFlip : card[0].id} })
+	      app.getBoardScore()
+	    }
+	    cardHolders.on('click',showCard)
+	  },
+	  getBoardScore: function(){
+	    let cards = app.board.find('.card')
+	    let boardScore = app.board.find('#boardScore')
+	    let currentScore = {let: boardScore.html()}
+	    let score = 0
+	    function tallyScore(){
+	      if($(this).data("flipped")){
+	       let value = $(this).find("b").html()
+	       score += parseInt(value)
+	     }
+	   }
+	   $.each(cards, tallyScore)      
+	   TweenMax.to(currentScore, 1, {
+	    let: score, 
+	    onUpdate: function () {
+	      boardScore.html(Math.round(currentScore.let))
+	    },
+	    ease: Power3.easeOut,
+	  });
+	 },
+	 awardPoints: function(num){
+	  var num          = $(this).attr("data-team")
+	  let boardScore   = app.board.find('#boardScore')
+	  let currentScore = {let: parseInt(boardScore.html())}
+	  let team         = app.board.find("#team"+num)
+	  let teamScore    = {let: parseInt(team.html())}
+	  let teamScoreUpdated = (teamScore.let + currentScore.let)
+	  console.log(team[0].id)
+	  
+	  socket.emit('points',{room:roomNum, points:{teamID:team[0].id, pts:teamScoreUpdated}})
+	  
+	  TweenMax.to(teamScore, 1, {
+	    let: teamScoreUpdated, 
+	    onUpdate: function () {
+	      team.html(Math.round(teamScore.let))
+	    },
+	    ease: Power3.easeOut,
+	  });
+	  
+	  TweenMax.to(currentScore, 1, {
+	    let: 0, 
+	    onUpdate: function () {
+	      boardScore.html(Math.round(currentScore.let))
+	    },
+	    ease: Power3.easeOut,
+	  });
+	  app.changeQuestion()
+	},
+	changeQuestion: function(){
+	  app.currentQ++
+	  app.makeQuestion(app.currentQ)
+	},
+	  // Inital function
+	  init: function(){
+	    $.getJSON(app.jsonFile, app.jsonLoaded)
+	    app.board.find('#newQuestion' ).on('click', app.changeQuestion)
+	    app.board.find('#awardTeam1'  ).on('click', app.awardPoints)
+	    app.board.find('#awardTeam2'  ).on('click', app.awardPoints)
+	  }  
+	}
+	app.init()
 
-    TweenLite.set(cardHolders , {perspective:800})
-    TweenLite.set(cards       , {transformStyle:"preserve-3d"})
-    TweenLite.set(backs       , {rotationX:180})
-    TweenLite.set(cardSides   , {backfaceVisibility:"hidden"})
-
-    cards.data("flipped", false)
-    
-    function showCard(){
-      let card = $('.card', this) 
-      let flipped = $(card).data("flipped")
-      let cardRotate = (flipped)?0:-180;
-      TweenLite.to(card, 1, {rotationX:cardRotate, ease:Back.easeOut})
-      flipped = !flipped
-      $(card).data("flipped", flipped)
-      socket.emit('commands', { room: roomNum, command:{cardFlip : card[0].id} })
-      app.getBoardScore()
-    }
-    cardHolders.on('click',showCard)
-  },
-  getBoardScore: function(){
-    let cards = app.board.find('.card')
-    let boardScore = app.board.find('#boardScore')
-    let currentScore = {let: boardScore.html()}
-    let score = 0
-    function tallyScore(){
-      if($(this).data("flipped")){
-       let value = $(this).find("b").html()
-       score += parseInt(value)
-     }
-   }
-   $.each(cards, tallyScore)      
-   TweenMax.to(currentScore, 1, {
-    let: score, 
-    onUpdate: function () {
-      boardScore.html(Math.round(currentScore.let))
-    },
-    ease: Power3.easeOut,
-  });
- },
- awardPoints: function(num){
-  var num          = $(this).attr("data-team")
-  let boardScore   = app.board.find('#boardScore')
-  let currentScore = {let: parseInt(boardScore.html())}
-  let team         = app.board.find("#team"+num)
-  let teamScore    = {let: parseInt(team.html())}
-  let teamScoreUpdated = (teamScore.let + currentScore.let)
-  console.log(team[0].id)
-  
-  socket.emit('points',{room:roomNum, points:{teamID:team[0].id, pts:teamScoreUpdated}})
-  
-  TweenMax.to(teamScore, 1, {
-    let: teamScoreUpdated, 
-    onUpdate: function () {
-      team.html(Math.round(teamScore.let))
-    },
-    ease: Power3.easeOut,
-  });
-  
-  TweenMax.to(currentScore, 1, {
-    let: 0, 
-    onUpdate: function () {
-      boardScore.html(Math.round(currentScore.let))
-    },
-    ease: Power3.easeOut,
-  });
-},
-changeQuestion: function(){
-  app.currentQ++
-  app.makeQuestion(app.currentQ)
-},
-  // Inital function
-  init: function(){
-    $.getJSON(app.jsonFile, app.jsonLoaded)
-    app.board.find('#newQuestion' ).on('click', app.changeQuestion)
-    app.board.find('#awardTeam1'  ).on('click', app.awardPoints)
-    app.board.find('#awardTeam2'  ).on('click', app.awardPoints)
-  }  
 }
-app.init()
 
